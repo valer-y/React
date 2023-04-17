6127ea3650b95e6b5e12b8c099068aa0cbcd3918
valer-y <yazykov.valery@gmail.com>
2023-04-17 05:48:03 +0200

    Try props concept

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+
+    const expences = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    return (<div>
+        <h2>Let's get started!</h2>
+        <ExpenseItem title={expences[0].title}
+                     amount={expences[0].amount}
+                     date={expences[0].date}>
+        </ExpenseItem>
+        <ExpenseItem title={expences[1].title}
+                     amount={expences[1].amount}
+                     date={expences[1].date}>
+        </ExpenseItem>
+        <ExpenseItem title={expences[2].title}
+                     amount={expences[2].amount}
+                     date={expences[2].date}>
+        </ExpenseItem>
+        <ExpenseItem title={expences[3].title}
+                     amount={expences[3].amount}
+                     date={expences[3].date}>
+        </ExpenseItem>
+    </div>);
 }
 
 export default App;
